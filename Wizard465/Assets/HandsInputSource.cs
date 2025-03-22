081091f7af00846630210b0d189ab9347c717d85
jmaksuta <jmaksuta@colostate.edu>
2025-03-22 15:44:52 -0400

    current progress. added debug display. hands not working though.

---
+++ Wizard465/Assets/HandsInputSource.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.OpenXR.Input;
+
+namespace Assets
+{
+    public class HandsInputSource : InputPositionSource
+    {
+        public InputDevice leftHand;
+        public InputDevice rightHand;
+        public Vector3 leftHandPosition;
+        public Vector3 rightHandPosition;   
+
+        public override Vector3[] GetInputPosition()
+        {
+            return new Vector3[] { leftHandPosition, rightHandPosition };
+        }
+
+        public override int GetNumberOfInputs()
+        {
+            return 2;
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created 
+        public void Start()
+        {
+            leftHand = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+            rightHand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        }
+
+        public void Update()
+        {
+            UpdateHandPositions();
+        }
+
+        void UpdateHandPositions()
+        {
+            if (leftHand.isValid)
+            {
+                leftHand.TryGetFeatureValue(CommonUsages.devicePosition, out leftHandPosition);
+            }
+            else
+            {
+                leftHandPosition = Vector3.zero;
+            }
+            if (rightHand.isValid)
+            {
+                rightHand.TryGetFeatureValue(CommonUsages.devicePosition, out rightHandPosition);
+            }
+            else
+            {
+                rightHandPosition = Vector3.zero;
+            }
+        }
+    }
+}
